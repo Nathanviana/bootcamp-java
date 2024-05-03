2c51215b248ef8b2c0973e82e2e33be714a2a049
Nathanviana <viananathan03@gmail.com>
2024-05-03 19:44:25 -0300

    atualização de repositorio e adição de aulas

---
+++ AprendendoSintaxe/src/anatomiaJava/identação.java
@@ -0,0 +1,17 @@
+package anatomiaJava;
+
+public class identação {
+
+	public static void main(String[] args) {
+		// sem identação
+		int num = 5;
+		if (num>6) 
+		System.out.println("Aprovado");
+		else if (num==6)
+		System.out.println("Prova Final");
+		else
+		System.out.println("Reprovado");
+
+	}
+
+}
---
+++ AprendendoSintaxe/src/variaveisTipos/VariaveisTipos.java
@@ -0,0 +1,10 @@
+package variaveisTipos;
+
+public class VariaveisTipos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
