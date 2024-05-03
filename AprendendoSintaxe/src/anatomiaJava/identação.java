8360712a939fde9a8749bb01d4f7a68819e336b6
Nathanviana <viananathan03@gmail.com>
2024-05-03 19:52:08 -0300

    modified folders

--- AprendendoSintaxe/src/anatomiaJava/identação.java
+++ AprendendoSintaxe/src/anatomiaJava/identação.java
@@ -11,6 +11,16 @@ public class identação {
 		System.out.println("Prova Final");
 		else
 		System.out.println("Reprovado");
+		
+		// com identação
+		int num2 = 5;
+		if (num2 > 6)
+			System.out.println("Aprovado");
+		else if (num2 == 6)
+			System.out.println("Prova Final");
+		else
+			System.out.println("Reprovado");
+
 
 	}
 
--- AprendendoSintaxe/src/variaveisTipos/VariaveisTipos.java
+++ AprendendoSintaxe/src/variaveisTipos/VariaveisTipos.java
@@ -3,8 +3,19 @@ package variaveisTipos;
 public class VariaveisTipos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//tipos de variaveis
+		byte idade = 12; // byte menos dos tipos de variaveis
+		short anoAtual = 2024; // short segundo menos comportando de -32.768 até 32.767
+		int valor = 20000; // int um dos tipos mais usados 
+		long numeroAleatorio = 3232345; //long sendo o maior tipo que comporta mais.
+		
+		// Tipos decimais:
+        float PI = 3.14F; // Sempre acompanha um F no final do num para identificar na leitura de que é do tipo float e não do tipo double
+        double salario = 2390.90;
+        
+        // variaveis e constante
+        // ultiliza-se "final" para informar que aquela variavel não poderar ser alterada
+        final int SALARIO = 1200;
 	}
 
 }
