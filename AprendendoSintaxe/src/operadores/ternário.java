e4f0c96267cf81279e138c3de28c5f55ce2c59a7
Nathanviana <viananathan03@gmail.com>
2024-05-05 12:54:31 -0300

    aula operadores ternarios

---
+++ AprendendoSintaxe/src/operadores/ternário.java
@@ -0,0 +1,22 @@
+package operadores;
+
+public class ternário {
+
+	public static void main(String[] args) {
+		
+		// representadas por "?" e ":"
+		//<expressão condicional> ? <caso condição seja true> : <caso condição seja false>
+		
+		int a,b;
+		
+		a = 6;
+		b = 6;
+		
+		String resultado = a==b ? "verdadeiro" : "falso";
+		
+		System.out.println(resultado);
+		
+
+	}
+
+}
