c38bdfee3bb037b98072cfee556113daaaa575dd
Nathanviana <viananathan03@gmail.com>
2024-05-11 19:36:56 -0300

    Aula de estrutura condicional composta- java

---
+++ EstruturasDeCondicao/src/estruturacondicional/EstruturaComposta.java
@@ -0,0 +1,22 @@
+package estruturacondicional;
+
+import java.util.Scanner;
+
+public class EstruturaComposta {
+
+	public static void main(String[] args) {
+		Scanner texto = new Scanner(System.in);
+		
+		int nota = 0;
+		
+		System.out.println("Informe a nota do aluno: ");
+		nota = texto.nextInt();
+		
+		if(nota >= 7)
+			System.out.println("vc passou de ano");
+		else
+			System.out.println("vc não passou de ano");
+
+	}
+
+}
