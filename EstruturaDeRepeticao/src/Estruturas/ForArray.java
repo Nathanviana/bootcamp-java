88f16ba0735eee52081131db71af6ceb79e3a94f
Nathanviana <viananathan03@gmail.com>
2024-05-14 18:59:39 -0300

    aula de for com arrays

---
+++ EstruturaDeRepeticao/src/Estruturas/ForArray.java
@@ -0,0 +1,14 @@
+package Estruturas;
+
+public class ForArray {
+
+	public static void main(String[] args) {
+		String alunos[] = { "Nathan", "Lufe", "Noser", "Dival" };
+
+		for (int x = 0; x < alunos.length; x++) {
+			System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+		}
+
+	}
+
+}
