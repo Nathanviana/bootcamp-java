28f1843c6838ed4ff90aac200a577f1e9c4923f6
Nathanviana <viananathan03@gmail.com>
2024-05-14 14:29:27 -0300

    aula estrutura de repetição usando For

---
+++ EstruturaDeRepeticao/src/Estruturas/For.java
@@ -0,0 +1,19 @@
+package Estruturas;
+
+public class For {
+
+	public static void main(String[] args) {
+		// contador com For crescendo
+		for (int contador = 0; contador <= 20; contador++) {
+			System.out.println(contador);
+		}
+		System.out.println("normal");
+
+		// contador com For decrescendo
+		for (int contador2 = 10; contador2 >= 0; contador2--) {
+			System.out.println(contador2);
+		}
+		System.out.println("normal");
+	}
+
+}
