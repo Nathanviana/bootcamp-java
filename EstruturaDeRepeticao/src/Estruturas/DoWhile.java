2188d320b117434401b4ac5ce0065e7aa98d0aa6
Nathanviana <viananathan03@gmail.com>
2024-05-15 13:28:20 -0300

    aulas de while e do while - java

---
+++ EstruturaDeRepeticao/src/Estruturas/DoWhile.java
@@ -0,0 +1,20 @@
+package Estruturas;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		//do while 
+		// o programa vai se rodado e depois testado
+		
+		int cont = 0;
+		
+		do {
+			System.out.println("contando " + cont);
+			cont++;
+		}while(cont < 0);
+	
+		System.out.println("fim do programa");
+	}
+	
+}
---
+++ EstruturaDeRepeticao/src/Estruturas/While.java
@@ -0,0 +1,16 @@
+package Estruturas;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		int valor = 0;
+		
+		while(valor < 5) {
+			System.out.println("contando " + valor);
+			valor++;
+		}
+
+	}
+
+}
