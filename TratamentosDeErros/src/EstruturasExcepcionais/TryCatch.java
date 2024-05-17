df2a26703b5ef8d5167897e4ff2d75e05d40afb9
Nathanviana <viananathan03@gmail.com>
2024-05-16 22:30:09 -0300

    Aula de try - java

---
+++ TratamentosDeErros/src/EstruturasExcepcionais/TryCatch.java
@@ -0,0 +1,19 @@
+package EstruturasExcepcionais;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+		
+		Scanner text = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Digite seu nome: ");
+		String nome = scanner.next();
+		
+		
+		
+	}
+
+}
