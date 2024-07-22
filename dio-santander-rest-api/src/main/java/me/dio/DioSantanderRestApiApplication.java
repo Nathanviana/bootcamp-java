5701fd1b6ee86a90a4ef77ddd642e3158e575b59
Nathanviana <viananathan03@gmail.com>
2024-07-22 18:57:45 -0300

    RESTful API

---
+++ dio-santander-rest-api/src/main/java/me/dio/DioSantanderRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSantanderRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSantanderRestApiApplication.class, args);
+	}
+
+}
